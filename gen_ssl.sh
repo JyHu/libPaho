47190b71b894aa9f1c438bc5c817caef938a4a90
Jinyou Hu <hujinyou@itiger.com>
2023-07-06 21:47:38 +0800

    update arm64

--- gen_ssl.sh
+++ gen_ssl.sh
@@ -10,6 +10,8 @@ function LOG {
     echo ">> $1"
 }
 
+START_TIME=$(date +%s)
+
 # 清理资源
 LOG "Clear Resources ..."
 rm -rf ssl_build
@@ -51,8 +53,28 @@ function ARCH_BUILD {
     LOG "find openssl folder $SSL_SOURCE_FOLD"
     cd $SSL_SOURCE_FOLD
     
-    # 设置一下变量
-    ./Configure $1 --prefix=$SSL_BUILD_FOLD/$2 no-shared
+    #
+    # 设置一下编译选项，其中需要注意 no-asm 参数。
+    #
+    # 在编译 OpenSSL 时，no-asm 是一个配置选项，用于禁用特定平台的汇编优化。
+    # 下面是关于 no-asm 的一些重要信息：
+    # 1. 汇编优化：OpenSSL 使用汇编语言来实现某些算法的优化，以提高性能。这些汇编代码通常是针对
+    #   特定的处理器架构和操作系统优化的。
+    # 2. no-asm 选项：使用 no-asm 配置选项可以禁用 OpenSSL 中的汇编优化。这意味着在编译过程
+    #   中不会使用汇编代码来进行性能优化。通常情况下，禁用汇编优化可以增加可移植性，但可能会导致
+    #   性能下降。
+    # 3. 使用 no-asm 的情况：
+    #   - 平台不支持汇编优化：某些平台可能不支持或不兼容特定的汇编优化。在这种情况下，
+    #     使用 no-asm 可以确保 OpenSSL 能够在该平台上正常编译和运行，尽管性能可能会受到影响。
+    #   - 架构兼容性问题：如果您计划在不同的架构上使用相同的编译结果，可以使用 no-asm 以确保生
+    #     成的代码在不同架构之间具有更好的可移植性。
+    # 4. 默认情况：通常情况下，no-asm 并不是默认启用的，因为汇编优化可以提供显著的性能提升。
+    #   默认情况下，OpenSSL 会尝试使用汇编优化来提高性能。
+    #
+    # 综上所述，使用 no-asm 配置选项可以在编译 OpenSSL 时禁用汇编优化。这可能是出于可移植性或
+    # 平台兼容性的考虑。但请注意，禁用汇编优化可能会导致性能下降。在实际使用中，您可以根据您的需
+    # 求和目标平台选择是否启用或禁用 no-asm 选项。
+    ./Configure $1 --prefix=$SSL_BUILD_FOLD/$2 no-shared no-asm
     
     # 编译
     make
@@ -62,8 +84,8 @@ function ARCH_BUILD {
     rm -rf $SSL_SOURCE_FOLD
 }
 
+ARCH_BUILD darwin64-arm64 arm64
 ARCH_BUILD darwin64-x86_64-cc x86_64
-ARCH_BUILD darwin64-arm64-cc arm64
 
 ARM64_SSL="$SSL_BUILD_FOLD/arm64/lib/libssl.a"
 X86_64_SSL="$SSL_BUILD_FOLD/x86_64/lib/libssl.a"
@@ -103,4 +125,10 @@ lipo -create $ARM64_CRYPTO $X86_64_CRYPTO -output ./libssl/lib/libcrypto.a
 # 移动头文件
 mv $SSL_BUILD_FOLD/arm64/include ./libssl/include
 
- rm -rf $SSL_BUILD_FOLD
+rm -rf $SSL_BUILD_FOLD
+
+END_TIME=$(date +%s)
+EXEC_DURATION=$((END_TIME - START_TIME))
+LOG ""
+LOG "Build openssl in $EXEC_DURATION seconds."
+LOG ""
--- libPahoC.podspec
+++ libPahoC.podspec
@@ -14,28 +14,28 @@ Pod::Spec.new do |s|
   
   # 异步包
   s.subspec '3a' do |ss|
-    ss.source_files = "libPahoC/*.h"
-    ss.vendored_libraries = "libPahoC/libpaho-mqtt3a.a"
+    ss.source_files = "libPahoC/headers/*.h"
+    ss.vendored_libraries = "libPahoC/libs/libpaho-mqtt3a.a"
   end
  
   # 异步包+支持openssl
   s.subspec '3as' do |ss|
-    ss.source_files = "libPahoC/*.h"
+    ss.source_files = "libPahoC/headers/*.h"
     ss.dependency "libPahoC/ssl"
-    ss.vendored_libraries = "libPahoC/libpaho-mqtt3as.a"
+    ss.vendored_libraries = "libPahoC/libs/libpaho-mqtt3as.a"
   end
   
   # 同步包
   s.subspec '3c' do |ss|
-    ss.source_files = "libPahoC/*.h"
-    ss.vendored_libraries = "libPahoC/libpaho-mqtt3c.a"
+    ss.source_files = "libPahoC/headers/*.h"
+    ss.vendored_libraries = "libPahoC/libs/libpaho-mqtt3c.a"
   end
  
   # 同步包+支持openssl
   s.subspec '3cs' do |ss|
-    ss.source_files = "libPahoC/*.h"
+    ss.source_files = "libPahoC/headers/*.h"
     ss.dependency "libPahoC/ssl"
-    ss.vendored_libraries = "libPahoC/libpaho-mqtt3cs.a"
+    ss.vendored_libraries = "libPahoC/libs/libpaho-mqtt3cs.a"
   end
   
   # 异步paho需要的openssl依赖
