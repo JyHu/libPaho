a84859428a8455e100c66824041f4e2cb261fd15
Jinyou Hu <hujinyou@itiger.com>
2023-07-11 18:58:10 +0800

    调整结构

---
+++ libssl/libssl.h
@@ -0,0 +1,18 @@
+//
+//  libssl.h
+//  libssl
+//
+//  Created by Jo on 2023/7/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libssl.
+FOUNDATION_EXPORT double libsslVersionNumber;
+
+//! Project version string for libssl.
+FOUNDATION_EXPORT const unsigned char libsslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libssl/PublicHeader.h>
+
+
