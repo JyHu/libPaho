fdeb324ee314af49988509d06d5354f106fd5014
Jinyou Hu <hujinyou@itiger.com>
2023-07-11 11:29:50 +0800

    增加target

---
+++ CocoaPaho/CocoaPaho.h
@@ -0,0 +1,27 @@
+//
+//  CocoaPaho.h
+//  CocoaPaho
+//
+//  Created by Jo on 2023/7/11.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PahoClient.h"
+#import "PahoConstants.h"
+#import "PahoMessages.h"
+#import "PahoProperties.h"
+#import "PahoPublisher.h"
+#import "PahoResponse.h"
+#import "PahoSSLOptions.h"
+#import "PahoTopic.h"
+
+//! Project version number for CocoaPaho.
+FOUNDATION_EXPORT double CocoaPahoVersionNumber;
+
+//! Project version string for CocoaPaho.
+FOUNDATION_EXPORT const unsigned char CocoaPahoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPaho/PublicHeader.h>
+
+
